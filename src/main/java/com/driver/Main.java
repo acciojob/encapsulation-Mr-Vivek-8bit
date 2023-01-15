d774be821c1e5b06a44ea337cabb29e906b5f2b3
Mr-Vivek-8bit <viveksonu444@gmail.com>
2023-01-15 18:57:52 +0530

    solution one

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        rw.setName("xyz");
+        System.out.println(rw.getName());
+
+        // java: name has private access in com.driver.RWOnly
+    }
+
 }
